7b24319d7471f691314fbcb01d8b5049804bbade
elmira lotfi <elmiralotfi1375@gmail.com>
2022-06-28 19:09:16 +0430

    middle header added to project

--- my-app/src/Components/header/Header.js
+++ my-app/src/Components/header/Header.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 //icons
-import {faPhone,faUser} from '@fortawesome/free-solid-svg-icons';
+import {faPhone,faUser,faSearch,faShoppingCart} from '@fortawesome/free-solid-svg-icons';
 import { faFacebook,faInstagram,faTwitter,faPinterest} from "@fortawesome/free-brands-svg-icons"
 
 //Styles
 import styles from "./Header.module.css";
+
+//Images
+import logo from "../../assetes/images/logo.png"
 
 const Header = () => {
     return (
         <header className={styles.header}>
 
             {/* headerTop  Start*/}
-            <div className={styles.hederTop}>
+            <div className={styles.headerTop}>
             <div className={styles.container}>
                 <div className={styles.headerLeft}>
                 <div className={styles.alignItemsCenter}>
@@ -36,6 +39,37 @@ const Header = () => {
             </div>
         </div>
         {/* hederTop End */}
+
+        {/* headerMiddle */}
+        <div className={styles.headerMiddle}>
+            <div className={styles.container}>
+                {/* left */}
+            <div className={styles.headerLeft} style={{flex: "1 1 auto"}}>
+                <form action="">
+                    <button type="submit">
+                        <i style={{color:"#333", fontSize:"1.2rem"}}>
+                            <FontAwesomeIcon icon={faSearch}/>
+                        </i>
+                    </button>
+                   <input type="text" placeholder="Search product.." name="search"/>
+                </form>
+            </div>
+            {/* center */}
+            <div className={styles.headerCenter}>
+                    <div className={styles.logo}>
+                       <img src={logo} alt="logo"/>
+                       </div>
+                    </div>
+                    {/* Right */}
+                    <div className={styles.headerRight}>
+                        <div className={styles.shoppingCart}>
+                        <i>< FontAwesomeIcon icon={faShoppingCart}/></i>
+                        <span>0</span>
+                        </div>
+                    </div>
+            </div>
+
+        </div>
         </header>
     );
 };
